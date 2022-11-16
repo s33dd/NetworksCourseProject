1e5d8d0344f23ada90e0e1ade61610322f4ce730
s33dd <dsuhoverikov@gmail.com>
2022-11-16 02:31:31 +0200

    added simulation

--- Simulate/Program.cs
+++ Simulate/Program.cs
@@ -3,7 +3,8 @@
 namespace Simulate {
   internal class Program {
     static void Main(string[] args) {
-      Console.WriteLine("Hello World!");
+      Simulator simulator = new Simulator();
+      simulator.Simulate(100); // 100 is expected value, can be changed. Simulation will fluctuate near it.
     }
   }
 }
---
+++ Simulate/Simulator.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Threading;
+
+namespace Simulate {
+  internal class Simulator {
+    private int currentValue;
+    private bool isPaused;
+    public Simulator() { 
+      currentValue = 0;
+      isPaused = false;
+    }
+    public int CurrentValue {
+      get { return currentValue; }
+    }
+    public void Simulate(int expected) {
+      bool wasAnomaly = false;
+      int tmp = currentValue;
+      Random Randomizer = new Random();
+      while (!isPaused) {
+        Thread.Sleep(5000);
+        int anomaly = Randomizer.Next(0, 100);
+        if (anomaly > 95) {
+          if (!wasAnomaly) {
+            tmp = currentValue;
+            wasAnomaly = false;
+          }
+          if (Randomizer.Next(2) == 1) {
+            currentValue += Randomizer.Next(90, 100);
+          }
+          else {
+            currentValue += Randomizer.Next(-100, -90);
+          }
+          wasAnomaly = true;
+        }
+        else {
+          if (currentValue <= expected) {
+            currentValue = tmp;
+            currentValue += Randomizer.Next(-7,19);
+            tmp = currentValue;
+          }
+          else {
+            currentValue = tmp;
+            currentValue += Randomizer.Next(-13,4);
+            tmp = currentValue;
+          }
+        }
+        //Check for pause
+      }
+    }
+  }
+}
