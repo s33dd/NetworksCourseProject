99c64e472b910d23ac58ae983d704713e835ed5c
s33dd <dsuhoverikov@gmail.com>
2022-11-30 09:04:44 +0200

    approximating dll

---
+++ Approximating/LeastSquares.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections.Generic;
+
+namespace Approximating {
+  public class LeastSquares {
+    private double a;
+    private double b;
+    public LeastSquares() {
+      a = 0;
+      b = 0;
+    }
+    public void CramerMethod(List<double> x, List<double> y) {
+      double xSum = 0;
+      double ySum = 0;
+      double squareXSum = 0;
+      double xySum = 0;
+      double n = x.Count;
+      for (int i = 0; i < n; i++) {
+        xSum += x[i];
+        ySum += y[i];
+        squareXSum += x[i] * x[i];
+        xySum += x[i] + y[i];
+      }
+      double delta = squareXSum * n - xSum * xSum;
+      if (delta == 0) {
+        return;
+      }
+      double deltaA = xySum * n - xSum * ySum;
+      double deltaB = squareXSum * ySum - xySum * xSum;
+      a = deltaA / delta;
+      b = deltaB / delta;
+    }
+    public double GetLogarithmic(double x) {
+      return a * Math.Log(x) + b;
+    }
+    public double GetLinear(double x) {
+      return a * x + b;
+    }
+  }
+}
