ff791c1b6ba4c55297ee1bfb8719b9edd69fd18b
s33dd <dsuhoverikov@gmail.com>
2022-12-03 18:25:02 +0200

    removed unnecessary usings

--- DataHandler/Chart.xaml.cs
+++ DataHandler/Chart.xaml.cs
@@ -3,24 +3,12 @@ using LiveChartsCore;
 using LiveChartsCore.Defaults;
 using LiveChartsCore.SkiaSharpView;
 using LiveChartsCore.SkiaSharpView.Painting;
-using LiveChartsCore.SkiaSharpView.WPF;
 using SkiaSharp;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
-using System.Drawing.Drawing2D;
-using System.Linq;
-using System.Text;
 using System.Threading;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 
 namespace DataHandler {
   /// <summary>
--- DataHandler/Connection.cs
+++ DataHandler/Connection.cs
@@ -1,14 +1,10 @@
 ﻿using Approximating;
 using LiveChartsCore.Defaults;
 using System;
-using System.Collections.Generic;
 using System.Collections.ObjectModel;
-using System.Linq;
 using System.Net;
 using System.Net.Sockets;
-using System.Text;
 using System.Text.RegularExpressions;
-using System.Threading.Tasks;
 using System.Windows;
 
 namespace DataHandler {
